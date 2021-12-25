fdc84a560c01dbef35d8279ca9d1bb872ee3a8c6
Marko Marinovic <marinovic.marko@outlook.com>
2021-12-25 14:49:33 +0100

    end token owner update

--- scripts/erc-20/deploy-endemic-erc20.js
+++ scripts/erc-20/deploy-endemic-erc20.js
@@ -6,7 +6,9 @@ async function main() {
   console.log('Deploying Endemic ERC20 with the account:', deployer.address);
 
   const EndemicToken = await ethers.getContractFactory('EndemicToken');
-  const endemicToken = await EndemicToken.deploy();
+  const endemicToken = await EndemicToken.deploy(
+    '0xcF96Ed58395d55d6bd0c470f7ed064741119cbC5'
+  );
   await endemicToken.deployed();
 
   console.log('Endemic ERC20 deployed to:', endemicToken.address);
