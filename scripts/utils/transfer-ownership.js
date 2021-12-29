e55db44bae10b70f9145bc357a4006c08ccf7250
Marko Marinovic <marinovic.marko@outlook.com>
2021-12-29 19:08:20 +0100

    added mainnet contracts

--- scripts/utils/transfer-ownership.js
+++ scripts/utils/transfer-ownership.js
@@ -4,18 +4,17 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log('Transfering ownership');
 
-  const newOwner = '0xF15B146d0251dB2fA9a4170fFFd6d587e009694A';
+  const newOwner = '0xcF96Ed58395d55d6bd0c470f7ed064741119cbC5';
 
   const addresses = {
-    contractRegistryProxy: '0x1BE1744a1d718C1E42dA5BD1e79639Ae6DFbEb58',
-    endemicMasterKeyProxy: '0x6D27C84EC245A7865718e1CDb7D1aE0EF4B6f08E',
-    feeProviderProxy: '0xb00BB669eb0953144caE73cf4049F59B6d358203',
-    endemicNftProxy: '0x599F825A6cBAdA1c8eB972F2ebb6780576d11B96',
-    endemicCollection: '0x1F081B956f670D0fbf6d02b0439e802540bA1aBD',
-    marketplaceProxy: '0xDd29A2E65c01B75d74A53a469bF90371697846BF',
-    endemicNftBeacon: '0xaFf28326DB64f2f02a7788A047E525B047c0A525',
-    endemicNftFactory: '0xa5572e8558b2CCf6Cb7f05f04f3803dc577966F1',
-    bidProxy: '0x7CDdB0f2defc12205693454a6B6581702e3C7924',
+    contractRegistryProxy: '0xB2d6c6D02f9b6C68c8Aa5fdb455f0feB008D3107',
+    endemicMasterKeyProxy: '0x97cb197f862173a0f4c0B9Fda3272b56464578cc',
+    feeProviderProxy: '0x3853676279Ada77826afDEdE6a815D5250A1867A',
+    endemicNftProxy: '0xCd75e540157E04b0a7f1E347d21dED2FF748AD0f',
+    endemicCollection: '0x329b61bF16aDd14863c1C154614888F14303169c',
+    endemicNftBeacon: '0xAfAF30cB1215e344088296e058c7694bAeBAe1E9',
+    marketplaceProxy: '0x5f89c1bBbCAc22fc15aC3074c0CfeC6bcF117FE5',
+    endemicNftFactory: '0x7e4fD7d4bb0e31A14B76a396F840b6FE08A51da3',
   };
 
   console.log('Transfering contract registry ownership');
@@ -64,10 +63,10 @@ async function main() {
   const endemicNFTFactory = await Endemic.attach(addresses.endemicNftFactory);
   await endemicNFTFactory.transferOwnership(newOwner);
 
-  console.log('Transfering bid ownership');
-  const Bid = await ethers.getContractFactory('Bid');
-  const bid = await Bid.attach(addresses.bidProxy);
-  await bid.transferOwnership(newOwner);
+  // console.log('Transfering bid ownership');
+  // const Bid = await ethers.getContractFactory('Bid');
+  // const bid = await Bid.attach(addresses.bidProxy);
+  // await bid.transferOwnership(newOwner);
 }
 
 main()
